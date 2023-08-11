9089956b49ab46014a0c361df288569939cd4e9c
ckoon-infopro <139832404+ckoon-infopro@users.noreply.github.com>
2023-08-11 14:44:01 +0800

    Close connection after getting fingerprint from server, also added connection retry on socket timeout up to 3 times

--- src/android/nl/xservices/plugins/SSLCertificateChecker.java
+++ src/android/nl/xservices/plugins/SSLCertificateChecker.java
@@ -6,21 +6,28 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import javax.net.ssl.HttpsURLConnection;
+
 import javax.security.cert.CertificateException;
 import java.io.IOException;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateEncodingException;
 
+import android.util.Log;
+
 public class SSLCertificateChecker extends CordovaPlugin {
 
   private static final String ACTION_CHECK_EVENT = "check";
-  private static char[] HEX_CHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+  private static char[] HEX_CHARS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+  public static final int MAX_RETRIES = 3;
+  public static final int TIMEOUT = 30000;
 
   @Override
-  public boolean execute(final String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
+  public boolean execute(final String action, final JSONArray args, final CallbackContext callbackContext)
+      throws JSONException {
     if (ACTION_CHECK_EVENT.equals(action)) {
       cordova.getThreadPool().execute(new Runnable() {
         public void run() {
@@ -28,7 +35,7 @@ public class SSLCertificateChecker extends CordovaPlugin {
             final String serverURL = args.getString(0);
             final JSONArray allowedFingerprints = args.getJSONArray(2);
             final String serverCertFingerprint = getFingerprint(serverURL);
-            for (int j=0; j<allowedFingerprints.length(); j++) {
+            for (int j = 0; j < allowedFingerprints.length(); j++) {
               if (allowedFingerprints.get(j).toString().equalsIgnoreCase(serverCertFingerprint)) {
                 callbackContext.success("CONNECTION_SECURE");
                 return;
@@ -38,25 +45,55 @@ public class SSLCertificateChecker extends CordovaPlugin {
           } catch (Exception e) {
             e.printStackTrace();
             callbackContext.error("CONNECTION_NOT_SECURE");
-            //callbackContext.error("CONNECTION_FAILED. Details: " + e.getMessage());
+            // callbackContext.error("CONNECTION_FAILED. Details: " + e.getMessage());
           }
         }
       });
       return true;
     } else {
-      callbackContext.error("sslCertificateChecker." + action + " is not a supported function. Did you mean '" + ACTION_CHECK_EVENT + "'?");
+      callbackContext.error("sslCertificateChecker." + action + " is not a supported function. Did you mean '"
+          + ACTION_CHECK_EVENT + "'?");
       return false;
     }
   }
 
-  private static String getFingerprint(String httpsURL) throws IOException, NoSuchAlgorithmException, CertificateException, CertificateEncodingException {
-    final HttpsURLConnection con = (HttpsURLConnection) new URL(httpsURL).openConnection();
-    con.setConnectTimeout(30000);
-    con.connect();
-    final Certificate cert = con.getServerCertificates()[0];
-    final MessageDigest md = MessageDigest.getInstance("SHA256");
-    md.update(cert.getEncoded());
-    return dumpHex(md.digest());
+  private static String getFingerprint(String httpsURL)
+      throws IOException, NoSuchAlgorithmException, CertificateException, CertificateEncodingException {
+    Log.d("SSLCertificateChecker", "httpsURL: " + httpsURL);
+
+    String fingerprint = null;
+
+    HttpsURLConnection con = null;
+
+    boolean hasTimeout = true;
+    int retries = 0;
+
+    while (hasTimeout && retries < MAX_RETRIES) {
+      try {
+        con = (HttpsURLConnection) new URL(httpsURL).openConnection();
+        con.setConnectTimeout(TIMEOUT);
+        con.setReadTimeout(TIMEOUT);
+        con.connect();
+
+        final Certificate cert = con.getServerCertificates()[0];
+        final MessageDigest md = MessageDigest.getInstance("SHA256");
+        md.update(cert.getEncoded());
+        fingerprint = dumpHex(md.digest());
+
+        hasTimeout = false;
+
+      } catch (SocketTimeoutException e) {
+        retries++;
+        Log.e("SocketTimeoutException", "Retrying: " + retries);
+        if (retries == MAX_RETRIES) {
+          throw e;
+        }
+      } finally {
+        con.disconnect();
+      }
+    }
+
+    return fingerprint;
   }
 
   private static String dumpHex(byte[] data) {
